a0d4ff22ccef174dfca18723cbedffde8090ce76
PiotrBerebecki <petebere@gmail.com>
2016-09-07 16:54:36 +0200

    Initial setup for redux

---
+++ client/reducers/posts.js
@@ -0,0 +1,14 @@
+function posts(state = [], action) {
+  console.log('The post will change');
+  console.log(state, action);
+  return state;
+  
+  // switch (action.type) {
+  //   case 'INCREMENT_LIKES':
+  //     return {
+        
+  //     };
+  // }
+}
+
+export default posts;
